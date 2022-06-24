a25cf3719b60a6acec245ff25ec82131e1f7321c
Harshil Ramanuj <harshil.r@simformsolutions.com>
2022-06-24 15:42:31 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RelayComponent } from './relay/relay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelayComponent
   ],
   imports: [
     BrowserModule,
